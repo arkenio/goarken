fff9749e3e0df79151bd6c68ca2cd3eea963a005
Damien Metzler <dmetzler@nuxeo.com>
2016-04-06 10:57:56 +0200

    NXIO-501 migrate fleet driver to new etcd client

--- drivers/fleet.go
+++ drivers/fleet.go
@@ -3,7 +3,6 @@ package drivers
 
 
 import (
-	"github.com/coreos/go-etcd/etcd"
 	. "github.com/arkenio/goarken/model"
 	"os/exec"
 	"os"
@@ -16,12 +15,12 @@ import (
 
 
 type FleetServiceDriver struct {
-	client *etcd.Client
+	etcdAddress string
 }
 
 
-func NewFleetServiceDriver(client *etcd.Client) *FleetServiceDriver {
-	return &FleetServiceDriver{client}
+func NewFleetServiceDriver(etcdAddress string ) *FleetServiceDriver {
+	return &FleetServiceDriver{etcdAddress}
 }
 
 func (f *FleetServiceDriver) Create(s *Service, startOnCreate bool) (interface{},error) {
@@ -30,39 +29,39 @@ func (f *FleetServiceDriver) Create(s *Service, startOnCreate bool) (interface{}
 
 
 func (f *FleetServiceDriver) Start(s *Service) (interface{},error) {
-	err := f.fleetcmd(s, "start", f.client)
+	err := f.fleetcmd(s, "start")
 	return s,err
 }
 
 func (f *FleetServiceDriver) Stop(s *Service) (interface{},error) {
-	err := f.fleetcmd(s, "stop", f.client)
+	err := f.fleetcmd(s, "stop")
 	return s,err
 }
 
 
 func (f *FleetServiceDriver) Passivate(s *Service) (interface{},error) {
 	log.Info(fmt.Sprintf("Passivating service %s",s.Name))
-	err := f.fleetcmd(s, "destroy", f.client)
+	err := f.fleetcmd(s, "destroy")
 	if err != nil {
 		return s,err
 	}
+	
+	//TODO make it at the model level
+	//statusKey := s.NodeKey + "/status"
+	//responseCurrent, error := f.client.Set(statusKey+"/current", PASSIVATED_STATUS, 0)
+	//if error != nil && responseCurrent == nil {
+	//	log.Errorf("Setting status current to 'passivated' has failed for Service "+s.Name+": %s", error)
+	//}
 
-	statusKey := s.NodeKey + "/status"
-
-	responseCurrent, error := f.client.Set(statusKey+"/current", PASSIVATED_STATUS, 0)
-	if error != nil && responseCurrent == nil {
-		log.Errorf("Setting status current to 'passivated' has failed for Service "+s.Name+": %s", error)
-	}
-
-	response, error := f.client.Set(statusKey+"/expected", PASSIVATED_STATUS, 0)
-	if error != nil && response == nil {
-		log.Errorf("Setting status expected to 'passivated' has failed for Service "+s.Name+": %s", error)
-	}
+	//response, error := f.client.Set(statusKey+"/expected", PASSIVATED_STATUS, 0)
+	//if error != nil && response == nil {
+	//	log.Errorf("Setting status expected to 'passivated' has failed for Service "+s.Name+": %s", error)
+	//}
 	return s, nil
 }
 
 func (f *FleetServiceDriver) Destroy(s *Service)  error {
-	err := f.fleetcmd(s, "destroy", f.client)
+	err := f.fleetcmd(s, "destroy")
 	return err
 }
 
@@ -71,11 +70,10 @@ func  unitNameFromService(s *Service) string {
 }
 
 
-func (f *FleetServiceDriver) fleetcmd(s *Service, command string, client *etcd.Client) error {
+func (f *FleetServiceDriver) fleetcmd(s *Service, command string) error {
 	//TODO Use fleet's REST API
-	etcdAddress := client.GetCluster()[0]
-
-	cmd := exec.Command("/usr/bin/fleetctl", "--endpoint="+etcdAddress, command, unitNameFromService(s))
+	
+	cmd := exec.Command("/usr/bin/fleetctl", "--endpoint="+f.etcdAddress, command, unitNameFromService(s))
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
